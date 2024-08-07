51c03ede42c46c719922ccd96f47477e09d0751d
Tim Gerry Geerts <timgerry.geerts@infosupport.com>
2024-08-07 10:07:18 +0200

    Getting started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Pokemon } from '../models/pokemon.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'pokemon-list-4';
+  title = 'Pokémon list';
+  pokemonList: Pokemon[] = [
+    { name: 'charmander', type: 'fire', attack: 'ember', level: 5 },
+    { name: 'bulbasaur', type: 'grass', type2: 'poison', attack: 'razor leaf', level: 5 },
+    { name: 'squirtle', type: 'water', attack: 'water gun', level: 5 }
+  ]
+
+  getPokemonImage(pokemon: Pokemon): string {
+    return `https://img.pokemondb.net/artwork/avif/${pokemon.name}.avif`;
+  }
+
+  attack(pokemon: Pokemon) {
+    window.alert(`${pokemon.name} used ${pokemon.attack}!`);
+  }
 }
---
+++ src/models/pokemon.model.ts
@@ -0,0 +1,7 @@
+export interface Pokemon {
+    name: string
+    type: string
+    type2?: string
+    attack: string
+    level: number
+}
