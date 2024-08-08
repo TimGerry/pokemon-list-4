2d1eb62f492e48fe72e974347f7108cb4fb3d1cb
Tim Gerry Geerts <timgerry.geerts@infosupport.com>
2024-08-08 11:44:58 +0200

    Dependency Injection: fix url

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
-const BASE_URL = environment.baseUrl;
+const BASE_URL = environment.baseUrl + '/pokemon';
 
 @Injectable({
   providedIn: 'root'
