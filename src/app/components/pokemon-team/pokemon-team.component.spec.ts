84faa9f0b236d60535ec35b36f237d2c665edc73
Tim Gerry Geerts <timgerry.geerts@infosupport.com>
2024-08-08 11:12:31 +0200

    Integration Testing: SpyObj

--- src/app/components/pokemon-team/pokemon-team.component.spec.ts
+++ src/app/components/pokemon-team/pokemon-team.component.spec.ts
@@ -3,28 +3,27 @@ import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angul
 import { PokemonTeamComponent } from './pokemon-team.component';
 import { PokemonService } from '../../services/pokemon.service';
 import { of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
 
 describe('PokemonTeamComponent', () => {
   let component: PokemonTeamComponent;
   let fixture: ComponentFixture<PokemonTeamComponent>;
   let nativeEl: HTMLElement;
-  let pokemonService: PokemonService;
+  let pokemonServiceSpy: jasmine.SpyObj<PokemonService>;
 
   beforeEach(async () => {
+    pokemonServiceSpy = jasmine.createSpyObj(PokemonService, ['getAll']);
+
     await TestBed.configureTestingModule({
       imports: [PokemonTeamComponent],
-        providers: [
-          { provide: HttpClient, useValue: null }
-        ]
+      providers: [
+        { provide: PokemonService, useValue: pokemonServiceSpy }
+      ]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(PokemonTeamComponent);
     component = fixture.componentInstance;
     nativeEl = fixture.nativeElement;
-
-    pokemonService = TestBed.inject(PokemonService);
   });
 
   it('should create', () => {
@@ -37,8 +36,8 @@ describe('PokemonTeamComponent', () => {
       { name: 'bulbasaur', type: 'grass', type2: 'poison', attack: 'razor leaf', level: 5 },
       { name: 'squirtle', type: 'water', attack: 'water gun', level: 5 }
     ];
-    spyOn(pokemonService, 'getAll').and.returnValue(of(testData));
-    
+    pokemonServiceSpy.getAll.and.returnValue(of(testData));
+
     // act
     fixture.detectChanges();
     await fixture.whenStable();
@@ -50,36 +49,36 @@ describe('PokemonTeamComponent', () => {
   });
 
   // fdescribe('Async test playground', () => {
-    // FAILS IN AFTER ALL
-    // it('should run async 1', () => {
-    //   const p = new Promise(res => {
-    //     setTimeout(() => res(42));
-    //   });
-    //   p.then(num => expect(num).toBe(0));
-    // });
+  // FAILS IN AFTER ALL
+  // it('should run async 1', () => {
+  //   const p = new Promise(res => {
+  //     setTimeout(() => res(42));
+  //   });
+  //   p.then(num => expect(num).toBe(0));
+  // });
 
-    // FAILS NORMALLY
-    // it('should run async', async () => {
-    //   const p = new Promise(res => setTimeout(() => res(42)));
-    //   const num = await p;
-    //   expect(num).toBe(0);
-    // });
+  // FAILS NORMALLY
+  // it('should run async', async () => {
+  //   const p = new Promise(res => setTimeout(() => res(42)));
+  //   const num = await p;
+  //   expect(num).toBe(0);
+  // });
 
-    // it('should run async with waitForAsync', waitForAsync(() => {
-    //   //                                         👆
-    //   const p = new Promise(res => setTimeout(() => res(42)));
-    //   p.then(num => expect(num).toBe(0));
-    // }));
+  // it('should run async with waitForAsync', waitForAsync(() => {
+  //   //                                         👆
+  //   const p = new Promise(res => setTimeout(() => res(42)));
+  //   p.then(num => expect(num).toBe(0));
+  // }));
 
-    // it('should run async with fakeAsync', fakeAsync(() => {
-    //   //                                    👆
-    //   let val = 0;
-    //   setTimeout(() => (val = 42), 10000);
-    //   setTimeout(() => (val = 0), 20000);
-    //   tick(10000);
-    //   expect(val).toBe(42);
-    //   tick(10000);
-    //   expect(val).toBe(0);
-    // }));
+  // it('should run async with fakeAsync', fakeAsync(() => {
+  //   //                                    👆
+  //   let val = 0;
+  //   setTimeout(() => (val = 42), 10000);
+  //   setTimeout(() => (val = 0), 20000);
+  //   tick(10000);
+  //   expect(val).toBe(42);
+  //   tick(10000);
+  //   expect(val).toBe(0);
+  // }));
   // });
 });
